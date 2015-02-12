b833cecbc96ffb92410022639d52e0c3c67bb1ae
Mišo <miso@fykos.cz>
2015-02-12 18:31:46 +0100

    repaint

--- helper.php
+++ helper.php
@@ -19,10 +19,6 @@ class helper_plugin_fksimageshow extends DokuWiki_Plugin {
         $this->FKS_helper = $this->loadHelper('fkshelper');
     }
 
-   
-
-   
-
     function resizeImage($image) {
         list($w, $h) = getimagesize($image);
         $k = $w / 320.0;
@@ -31,10 +27,6 @@ class helper_plugin_fksimageshow extends DokuWiki_Plugin {
         $size = '?w=320' . '&h=' . $hnew . '&tok=' . $this->getRand();
         return $size;
     }
-
-    
-
-    
 
 }
 
@@ -104,27 +96,101 @@ class fksimage extends helper_plugin_fksimageshow {
      */
     private $file_ext;
 
+    /**
+     *
+     * @var array of new color
+     */
+    private $color_new;
+
+    /**
+     *
+     * @var array of old color
+     */
+    private $color_old;
+
+    /**
+     *
+     * @var int new red value
+     */
+    private $new_red;
+
+    /**
+     *
+     * @var int new green value
+     */
+    private $new_green;
+
+    /**
+     *
+     * @var int new blue value
+     */
+    private $new_blue;
+
+    /**
+     *
+     * @var int new alpha value
+     */
+    private $new_alpha;
+
+    /**
+     *
+     * @var int old red value
+     */
+    private $old_red;
+
+    /**
+     *
+     * @var int old rgreen value
+     */
+    private $old_green;
+
+    /**
+     *
+     * @var int old blue value
+     */
+    private $old_blue;
+
+    /**
+     *
+     * @var int old alpha value
+     */
+    private $old_alpha;
+
     public function __construct() {
+
         global $conf;
         $this->ini_file = DOKU_INC . 'lib/tpl/' . $conf['template'] . '/style.ini';
         $this->ini_time = filemtime($this->ini_file);
         $this->ini_atr = parse_ini_file($this->ini_file);
-
-
-
         $this->season_name = $this->ini_atr['__season__'];
         $this->season_dir = 'lib/tpl/' . $conf['template'] . '/images/season/' . $this->season_name . '/';
         $this->default_dir = 'lib/tpl/' . $conf['template'] . '/images/season/default/';
     }
 
-    public function _create($file, $type) {
+    /**
+     * 
+     * @param type $file name of file without ext
+     * @param string $type od file jpg/png
+     * @return void
+     */
+    public function _create($file, $type = null) {
 
-        $this->file_ext = $type;
-        $this->file_name = $file;
+        if ($type) {
+            $this->file_ext = $type;
+            $this->file_name = $file;
+        } else {
+            $path = pathinfo($file);
+            $this->file_ext = $path['extension'];
+            $this->file_name = $path['filename'];
+        }
+
         $this->file_patch = DOKU_INC . $this->season_dir . $file . '.' . $type;
         $this->file_time = @filemtime($this->file_patch);
     }
 
+    /**
+     *@return void 
+     */
     public function _colorize() {
 
         if (!file_exists(DOKU_INC . $this->season_dir)) {
@@ -134,8 +200,13 @@ class fksimage extends helper_plugin_fksimageshow {
         if ((!file_exists($this->file_patch) || ($this->file_time < $this->ini_time))) {
             $this->_fks_colorize_img();
         }
+        return;
     }
 
+    /**
+     * 
+     * @return boolean
+     */
     private function _fks_colorize_img() {
 
         $this->default_file = DOKU_INC . $this->default_dir . $this->file_name . '.' . $this->file_ext;
@@ -146,27 +217,15 @@ class fksimage extends helper_plugin_fksimageshow {
         } else {
             return;
         }
-        list($w, $h) = getimagesize($this->default_file);
+
         if (preg_match('/radioactive/i', $this->file_name)) {
             $style_rgb = hexdec($this->ini_atr['__vyfuk_back__']);
         } else {
             $style_rgb = hexdec($this->ini_atr['__vyfuk_head__']);
         }
-        $style_color = imagecolorsforindex($im, $style_rgb);
-        for ($i = 0; $i < $w; $i++) {
-            for ($j = 0; $j < $h; $j++) {
-                $rgb = imagecolorat($im, $i, $j);
-                $colors = imagecolorsforindex($im, $rgb);
-                if ($colors['alpha'] != 127) {
-                    if ($colors["red"] != 255 || $colors["green"] != 255 || $colors["blue"] != 255) {
-                        if ($colors["red"] != $style_color["red"] || $colors["green"] != $style_color["green"] || $colors["blue"] != $style_color["blue"]) {
-                            $color = imagecolorallocate($im, $style_color["red"], $style_color["green"], $style_color["blue"]);
-                            imagesetpixel($im, $i, $j, $color);
-                        }
-                    }
-                }
-            }
-        }
+        $this->_fks_repaint_img($im, $style_rgb);
+
+
         ob_start();
         if ($this->file_ext == "png") {
             imagesavealpha($im, true);
@@ -182,7 +241,15 @@ class fksimage extends helper_plugin_fksimageshow {
         return true;
     }
 
-    public static function _fks_season_image($file, $type, $dis_scan = false) {
+    /**
+     * 
+     * @global type $conf
+     * @param string $file file name or full name of file
+     * @param string $type extenson of file
+     * @param bool $dis_scan fix for inf cycle
+     * @return string path of repaint file
+     */
+    public static function _fks_season_image($file, $type = null, $dis_scan = false) {
         global $conf;
 
         $image = new fksimage;
@@ -200,6 +267,52 @@ class fksimage extends helper_plugin_fksimageshow {
             }
         }
         return DOKU_BASE . 'lib/tpl/' . $conf['template'] . '/images/season/' . $image->season_name . '/' . $file . '.' . $type;
+    }
+
+    /**
+     * 
+     * @param image $im
+     * @param color $style_rgb
+     * @return void
+     */
+    private function _fks_repaint_img(&$im, $style_rgb) {
+        list($w, $h) = getimagesize($this->default_file);
+        $this->color_new = imagecolorsforindex($im, $style_rgb);
+        for ($i = 0; $i < $w; $i++) {
+            for ($j = 0; $j < $h; $j++) {
+                $rgb = imagecolorat($im, $i, $j);
+                $this->color_old = imagecolorsforindex($im, $rgb);
+                foreach (array('red', 'green', 'blue', 'alpha')as $value) {
+                    $this->${"new_" . $value} = $this->color_new[$value];
+                    $this->${"old_" . $value} = $this->color_old[$value];
+                }
+                if ($this->_can_paint()) {
+                    $color = imagecolorallocate($im, $this->new_red, $this->new_green, $this->new_blue);
+                    imagesetpixel($im, $i, $j, $color);
+                }
+            }
+        }
+        return;
+    }
+
+    /**
+     * 
+     * @return boolean
+     */
+    private function _can_paint() {
+
+
+        $white = (
+                ($this->old_red != 255) ||
+                ($this->old_green != 255) ||
+                ($this->old_blue != 255)
+                );
+        $same = (
+                ($this->old_red != $this->new_red) ||
+                ($this->old_green != $this->new_green) ||
+                ($this->old_blue != $this->new_blue));
+
+        return (boolean) ($this->old_alpha != 127) && $white && $same;
     }
 
 }
